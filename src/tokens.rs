6ae3a37f356941c043801fb006c680f4b3b92e7f
Hannes Bibel <hannes.bibel@gmail.com>
2024-01-07 11:28:02 +0100

    Fix not being able to parse identifiers with _ in them

--- src/tokens.rs
+++ src/tokens.rs
@@ -7,49 +7,53 @@ pub enum Token {
     Semicolon,
 }
 
-pub fn scan(source_code: String) -> Result<Vec<Token>, String> {
-    let mut tokens = Vec::new();
-    let mut current = vec![];
-    for ch in source_code.chars() {
-        let start_new_token: bool = current.is_empty()
-            || ch.is_whitespace()
-            || ch == '_' // TODO foo_bar is also a token
-            || ch == '('
-            || ch == ')'
-            || ch == ';'
-            || ch.is_alphanumeric() && !current.iter().all(|c: &char| c.is_alphanumeric());
-
-        if start_new_token && !current.is_empty() {
-            let token = to_token(&current)?;
-            tokens.push(token);
-            current.clear();
-        }
-
-        if !ch.is_whitespace() {
-            current.push(ch);
-        }
-    }
-    if !current.is_empty() {
-        let token = to_token(&current)?;
-        tokens.push(token);
-        current.clear();
-    }
-    Ok(tokens)
+enum State {
+    Init,
+    InAlphanum,
 }
 
-fn to_token(cs: &Vec<char>) -> Result<Token, String> {
-    match cs[..] {
-        ['_'] => Ok(Token::Underscore),
-        [';'] => Ok(Token::Semicolon),
-        ['('] => Ok(Token::OpenParen),
-        [')'] => Ok(Token::CloseParen),
-        _ if cs.iter().all(|c| c.is_alphanumeric())
-            && cs.get(0).map(|c| c.is_alphabetic()).unwrap() =>
-        {
-            Ok(Token::Alphanum(cs.iter().collect()))
+pub fn scan(source_code: String) -> Result<Vec<Token>, String> {
+    let mut tokens = Vec::new();
+    let mut charbuffer = vec![];
+    let mut state = State::Init;
+    for ch in source_code.chars() {
+        match (&state, ch) {
+            (State::Init, _) if ch.is_whitespace() => continue,
+            (State::Init, _) if ch.is_alphanumeric() => {
+                charbuffer.push(ch);
+                state = State::InAlphanum;
+            }
+            (State::Init, '(') => tokens.push(Token::OpenParen),
+            (State::Init, ')') => tokens.push(Token::CloseParen),
+            (State::Init, ';') => tokens.push(Token::Semicolon),
+            (State::Init, '_') => tokens.push(Token::Underscore),
+            (State::Init, _) => return Result::Err(format!("Invalid character '{}'", ch)),
+            (State::InAlphanum, _) if ch.is_alphanumeric() || ch == '_' => {
+                charbuffer.push(ch);
+            }
+            (State::InAlphanum, other) => {
+                tokens.push(Token::Alphanum(charbuffer.iter().collect()));
+                charbuffer.clear();
+
+                match other {
+                    ws if ws.is_whitespace() => {}
+                    '(' => tokens.push(Token::OpenParen),
+                    ')' => tokens.push(Token::CloseParen),
+                    ';' => tokens.push(Token::Semicolon),
+                    unexpected => {
+                        return Result::Err(format!("Invalid character '{}'", unexpected))
+                    }
+                }
+
+                state = State::Init;
+            }
         }
-        _ => Result::Err(format!("Invalid token '{}'", cs.iter().collect::<String>())),
     }
+    if !charbuffer.is_empty() {
+        let token = Token::Alphanum(charbuffer.iter().collect());
+        tokens.push(token);
+    }
+    Ok(tokens)
 }
 
 #[cfg(test)]
@@ -58,14 +62,36 @@ mod tests {
 
     #[test]
     fn test_one_statement() {
-        let source_code = String::from("println   (_);");
+        let source_code = String::from("print(_);");
         let expected = vec![
-            Token::Alphanum(String::from("println")),
+            Token::Alphanum(String::from("print")),
             Token::OpenParen,
             Token::Underscore,
             Token::CloseParen,
             Token::Semicolon,
         ];
+        let actual = scan(source_code).unwrap();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_superfluous_ws() {
+        let source_code = String::from("print   (_);");
+        let expected = vec![
+            Token::Alphanum(String::from("print")),
+            Token::OpenParen,
+            Token::Underscore,
+            Token::CloseParen,
+            Token::Semicolon,
+        ];
+        let actual = scan(source_code).unwrap();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_underscore_identifier() {
+        let source_code = String::from("foo_bar");
+        let expected = vec![Token::Alphanum(String::from("foo_bar"))];
         let actual = scan(source_code).unwrap();
         assert_eq!(actual, expected);
     }
